6dc2e7dd16e88922ad2a378edc45a17dfd7237a0
amirsh18-meet <amirshahbari@gmail.com>
2017-01-12 20:11:15 +0200

    stopping here

--- my_str.py
+++ my_str.py
@@ -1,5 +1,5 @@
 #FIX THE LINE BELOW
-class xyz : #<-----Replace xyz-make a new class, MyStr, that inherits from str
+class MyStr(str) : #<-----Replace xyz-make a new class, MyStr, that inherits from str
     """
     Build a subclass of str with some new, fun methods.
     """
@@ -13,9 +13,11 @@ class xyz : #<-----Replace xyz-make a new class, MyStr, that inherits from str
         :param num: number of exclamation points to add
         :returns: a string with num exclamation points added to the end
         """
-        return self+'!'*num
+        return str+'!'*num
 
     def replace(self, take_out, use_this):
+        
+       
         """
         Override the replace method of string.
         The new replace method is case-insensitive,
@@ -49,4 +51,6 @@ class xyz : #<-----Replace xyz-make a new class, MyStr, that inherits from str
         #       do this in as little as 1 line.
         #YOUR CODE BELOW:
         #################
-        
+        str_lower_case = self.lower()
+        return str_lower_case.replace(take_out.lower(), use_this)
+ 
